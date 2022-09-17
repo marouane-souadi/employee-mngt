94234fda0052e1850112d49744d3f8df4c411820
Marouane <m.souadi@gmail.com>
2022-09-17 18:18:23 +0100

    added auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+exports.loginRequired = (req, res, next)=>{
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        jwt.verify(token, config.SECRET_KEY, (err, decoded)=>{
+            if (decoded) {
+                console.log(decoded)
+                // TODO: Add expire and check TTL
+                next()
+            } else {
+                next({
+                    status : 401,
+                    message : "Please login first"
+                })
+            }
+        })
+    } catch(e) {
+        next({
+            status : 401,
+            message : "Please login first"
+        })
+    }
+}
